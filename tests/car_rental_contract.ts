7aa203789742b10b1860bc3fb37ec69449f2dc3c
Oleksandr_Husiev <ohusiev@icloud.com>
2025-03-04 23:35:28 +0100

    added test for addCar function

--- tests/car_rental_contract.ts
+++ tests/car_rental_contract.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CarRentalContract } from "../target/types/car_rental_contract";
+import { assert } from "chai";
 
 describe("car_rental_contract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.CarRentalContract as Program<CarRentalContract>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Adds a car", async () => {
+    const carAccount = anchor.web3.Keypair.generate();
+    const tx = await program.methods
+      .addCar(new anchor.BN(1), "Tesla Model S", new anchor.BN(100))
+      .accounts({
+        car: carAccount,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      } as any)
+      .signers([carAccount])
+      .rpc();
+
+    console.log("Transaction Signature (Initialize):", tx);
+
+    const car = await program.account.car.fetch(carAccount.publicKey);
+    console.log("Added car: ", car);
+
+    assert.equal(car.id.toNumber(), 1);
+    assert.equal(car.name, "Tesla Model S");
+    assert.equal(car.pricePerDay.toNumber(), 100);
+    assert.isNull(car.renterId);
+    assert.equal(car.rentEndDate.toNumber(), 0);
   });
 });
